f24b9ae2646245f900fa7f49d286adb7791c4dad
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-03-26 10:53:53 -0400

    feat: dateformatter

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
         Instant d03 = Instant.now();
 
 
-        LocalDate d04 = LocalDate.parse("2022-07-20");
-        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
-        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
-        Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
+        LocalDate d04 = LocalDate.parse("2025-01-20");
+        LocalDateTime d05 = LocalDateTime.parse("2025-01-20T01:30:26");
+        Instant d06 = Instant.parse("2025-01-20T01:30:26Z");
+        Instant d07 = Instant.parse("2025-01-20T01:30:26-03:00");
 
-        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDate d08 = LocalDate.parse("20/07/2025", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2025 07:30", fmt2);
 
         System.out.println("d01 = " + d01);
         System.out.println("d02 = " + d02);
@@ -28,5 +30,6 @@ public class Main {
         System.out.println("d06 = " + d06);
         System.out.println("d07 = " + d07);
         System.out.println("d08 = " + d08.toString());
+        System.out.println("d09 = " + d09);
     }
 }
