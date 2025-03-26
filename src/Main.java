841de4382bdf1b619423149fc3e6aa7e321e6eba
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-03-26 15:12:54 -0400

    fix warnings

--- src/Main.java
+++ src/Main.java
@@ -40,7 +40,7 @@ public class Main {
         System.out.println("d05 = " + d05);
         System.out.println("d06 = " + d06);
         System.out.println("d07 = " + d07);
-        System.out.println("d08 = " + d08.toString());
+        System.out.println("d08 = " + d08);
         System.out.println("d09 = " + d09);
         System.out.println("d10 = " + d10);
         System.out.println("d10 = " + d11);
