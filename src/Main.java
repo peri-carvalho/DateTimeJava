100d6e3d7d74f840b06f7394f64ccb76cdc5fa2a
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-03-26 15:00:16 -0400

    feat: calculate with dates

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
@@ -24,6 +26,13 @@ public class Main {
         LocalDate d10 = LocalDate.of(2025, 1,20);
         LocalDateTime d11 = LocalDateTime.of(2025,7,20,1,30);
 
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+
+
+
         System.out.println("d01 = " + d01);
         System.out.println("d02 = " + d02);
         System.out.println("d03 = " + d03.toString());
@@ -35,5 +44,60 @@ public class Main {
         System.out.println("d09 = " + d09);
         System.out.println("d10 = " + d10);
         System.out.println("d10 = " + d11);
+
+
+        //Convertendo data-hora para texto
+
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d05zone = " + d05.format(fmt4));
+        System.out.println("d06iso = " + fmt3.format(d06));
+        System.out.println("d06inst = " + fmt5.format(d06));
+
+        //
+
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
+
+        System.out.println("d04 dia = " + d04.getDayOfMonth());
+        System.out.println("d04 mes = " + d04.getMonthValue());
+        System.out.println("d04 ano = " + d04.getYear());
+
+        System.out.println("d05 hora = " + d05.getHour());
+        System.out.println("d05 min = " + d05.getMinute());
+
+        // Calculos
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7,ChronoUnit.DAYS);
+
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+
+        System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+        System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+
+        System.out.println("pastWeekInstant = " + pastWeekInstant);
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+//      Duration t1 = Duration.between(pastWeekLocalDate.atTime(0,0), d04.atTime(0,0));
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+        Duration t4 = Duration.between(d06, pastWeekInstant);
+
+
+        System.out.println("t1 dias = " + t1.toDays());
+        System.out.println("t2 dias = " + t2.toDays());
+        System.out.println("t3 dias = " + t3.toDays());
+        System.out.println("t4 dias = " + t4.toDays());
     }
 }
